37b7ec2e685e4bbffc004fea2c10da54166c1802
mariojr <marioccomp@gmail.com>
2026-08-24 00:36:50 -0300

    feat: starting project, add ui and auth components with semantic color variables

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    adonisjs({
+      /**
+       * Entrypoints of your application. Each entrypoint will
+       * result in a separate bundle.
+       */
+      entryPoints: ['resources/css/app.css', 'resources/js/app.js'],
+      assets: ['resources/assets/**'],
+
+      /**
+       * Paths to watch and reload the browser on file change
+       */
+      reload: ['resources/views/**/*.edge'],
+    }),
+  ],
+
+  server: {
+    watch: {
+      ignored: ['**/storage/**', '**/tmp/**'],
+    },
+  },
+})
